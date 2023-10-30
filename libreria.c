2906b8906bb6002f2a308b95b0691c7c30f0c747
Pbantolin12 <95373922+Pbantolin12@users.noreply.github.com>
2023-10-30 12:45:47 +0100

    Create libreria.c
    Se crea el archivo libreria.c
    
    En comentarios está puesto lo que tiene que hacer cada función más o menos.

---
+++ libreria.c
@@ -0,0 +1,31 @@
+#include <libreria.h>
+#include <stdio.h>
+
+//Muestra las N primeras líneas en la salida estándar recibidas por la entrada estándar
+int head(int n){ 
+    //For para ir leyendo las n líneas
+        //Imprimir por pantalla
+    //Fin del for
+}
+
+//Muestra las N últimas líneas en la salida estándar recibidas por la entrada estándar
+int tail(int n){ 
+    //For inverso para ir leyendo las n últimas líneas
+        //Imprimir por pantalla
+    //Fin dle for
+}
+
+//Muestra las N líneas más largas recibidas de mayor a menor, o todas ellas si hay menos de N líneas
+int longline(int n){
+    //If comprobando si hay menos de n líneas
+        //For para recorrer las líneas
+            //Imprimir línea
+        //Fin del for
+    //Else hay más de n líneas
+        //For para recorrer las líneas
+            //Comparar tamaño líneas e ir almacenándolas en un array
+        //Fin for
+        //For para recorrer el array de líneas ordenado por tamaño
+            //Imprimir línea
+        //Fin del for
+}
