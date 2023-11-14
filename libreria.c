0818ca5bbe75192096559cd613be22e0b552d743
David Paul Limaylla Ticlavilca <116211485+DavidPaul-LT@users.noreply.github.com>
2023-11-14 17:46:27 +0100

    Update libreria.c 3/3
    contenido de longLine + funciones para aplicar quicksort en longLine

--- libreria.c
+++ libreria.c
@@ -3,6 +3,44 @@
 #include "libreria.h"
 #include <stdio.h>
 
+// Función para intercambiar dos strings
+void swap(char** a, char** b) {
+    char* temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Función para particionar el array
+int partition(char* arr[], int low, int high) {
+    // El pivote es la longitud de la última string
+    int pivot = strlen(arr[high]);
+    int i = (low - 1);
+
+    // Recorre el array y mueve las strings más largas que el pivote al principio del array
+    for (int j = low; j <= high - 1; j++) {
+        if (strlen(arr[j]) >= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    // Coloca el pivote en su posición correcta
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// Función principal del algoritmo QuickSort
+void quickSort(char* arr[], int low, int high) {
+    if (low < high) {
+        // Encuentra la posición del pivote
+        int pi = partition(arr, low, high);
+
+        // Ordena recursivamente las dos mitades
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
 //Muestra las N primeras líneas en la salida estándar recibidas por la entrada estándar
 int head(int n){ 
     int lenght = 1024;                                             //Contine el máximo de carácteres que tendrá cada línea a leer
@@ -65,15 +103,52 @@ int tail(int n){
 
 //Muestra las N líneas más largas recibidas de mayor a menor, o todas ellas si hay menos de N líneas
 int longline(int n){
-    //If comprobando si hay menos de n líneas
-        //For para recorrer las líneas
-            //Imprimir línea
-        //Fin del for
-    //Else hay más de n líneas
-        //For para recorrer las líneas
-            //Comparar tamaño líneas e ir almacenándolas en un array
-        //Fin for
-        //For para recorrer el array de líneas ordenado por tamaño
-            //Imprimir línea
-        //Fin del for
+    int l = 1;                                                     //tamaño actual del array
+    int i = 0;                                                     //Índice actual
+    char** array;                                                  //Se declara un array de punteros a char.
+    int length = 1024;                                             //Tamaño máximo de cada línea.
+    char textO[length];                                            //Para almacenar el string recibido de la stdin
+    array = (char*) malloc(l*sizeof(char));                      //Se reserva memoria para el array.
+    // Control de error por asignación de memoria * 
+    if(array == NULL){                                             //*
+        fprintf(stderr, "Error al asignar memoria. \n");
+        exit(1);
+    }
+    array[i] = (char*) malloc(length * sizeof(char));              //Se reserva memoria para el primer elemento del array.
+    if(array[i] == NULL) {                                         //*
+        fprintf(stderr, "Error al asignar memoria al elemento.\n");
+        exit(1);
+    }
+    
+    // Se lee de la entrada estándar línea por línea.
+    while(fgets(textO, length, stdin) != NULL){
+        // Si el tamaño del array es menor que n(máximo de líneas requeridas), se incrementa el tamaño del array.
+        if(l < n){
+            l++;
+            i++;
+            array = (char*) realloc(array, l*sizeof(char));      //Se reserva memoria para el array (+1 tamaño).
+            if(array == NULL){                                     //*
+                fprintf(stderr, "Error al asignar memoria al array con un elemento nuevo. \n");
+                exit(1);
+            }
+            array[l-1] = (char*) malloc(length * sizeof(char));    //Se reserva memoria para el nuevo elemento del array.
+            if(array[l-1] == NULL) {                               //*
+                fprintf(stderr, "Error al asignar memoria al elemento nuevo.\n");
+                exit(1);
+            }
+        }
+        // Si el número de caractéres del string de la última posición del array es menor al de la línea recibida por el stdin 
+        if(strlen(array[i]) < strlen(textO)){
+            strcpy(array[i], textO);                               //copiar el valor de textO al array en la posicion i 
+            quickSort(array, 0, l-1);                              //Se ordena el array actual, dependiendo del tamaño de los strings high > low
+        }
+    }
+
+    // Se imprime el contenido del array.
+    for(int i = 0; i < l; i++){
+        printf("\n%s", array[i]);
+        free(array[i]);                                             //Se libera la memoria de cada elemento del array.
+    }
+    free(array);                                                    //Se libera la memoria del array.
+    return 0;
 }
