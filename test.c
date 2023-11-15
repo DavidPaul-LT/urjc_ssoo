3006a47abfc8f6a9f1d4b7916b935cb2aa48fd1f
Pbantolin12 <95373922+Pbantolin12@users.noreply.github.com>
2023-11-15 18:22:54 +0100

    Cambio en el programa test
    El anterior porgrama tes pasa a llamarse testMenu

--- test.c
+++ test.c
@@ -1,67 +1,45 @@
 #include "libreria.h"
 #include <stdio.h>
 
-int menu(){
-    int opcion; //Opcion del menu
+int main(int argc, char const *argv[], char *envp[]){
 
-    printf(" ______________________________________________\n");
-    printf("|_____________MENU_PROBAR_LIBRERIA_____________|\n");
-    printf("| 1. Introducir el numero de lineas a procesar |\n");
-    printf("| 2. Ver numero de lineas que se procesan      |\n");
-    printf("| 3. Head                                      |\n");
-    printf("| 4. Tail                                      |\n");
-    printf("| 5. Longline                                  |\n");
-    printf("| 6. Salir                                     |\n");
-    printf("|______________________________________________|\n");
-    printf("\n");
-    printf("--> Introduzca una opcion: ");
-    scanf("%d", &opcion);
-    printf("\n");
-    //Constrol de errores
-    if(opcion < 1 || opcion > 6){
-        printf("ERROR --> La opcion introducida es incorrecta");
-        printf("\n");
-        printf("--> Introduzca una opcion valida: ");
-        scanf("%d", &opcion);
-        printf("\n");
-    }
-    else
-        return opcion;
+    if(argc < 2 || argc > 3){ //Control de errores en el caso de que los parámetros sean erroneos
+        printf("ERROR --> Numero de argumentos erroneo\n");
+        return 1;
+    } 
+    else{
+            char funcionSel = argv[1][strlen(argv[1])]; //Seleccionamos la función introducida
+            printf("%s\n", funcionSel);
+            int lineas = 10; //El número de líneas a procesar por defecto es 10
 
-}
-
-void main(int argc, char const *argv[], char *envp[]){
-    int num = 5; //Valor que se le pasará a las funciones (5 por defecto)
-    int opc; //Opcion seleccionada del menu
-
-    opc = menu();
-    while(opc != 6){
-        switch (opc){
-        case 1:
-            printf("--> Introduzca el numero de lineas: ");
-            scanf("%d", &num);
-            printf("\n");
-            break;
-        case 2:
-            printf("El numero de lineas que se procesan es: %d\n", num);
-            printf(" \n");
-            break;
-        case 3:
-            printf("Ejecutando la funcion 'head'...\n");
-            head(num);
-            printf("\n");
-            break;
-        case 4:
-            printf("Ejecutando la funcion 'tail'...\n");
-            tail(num);
-            printf("\n");
-            break;
-        case 5:
-            printf("Ejecutando la funcion Longline...\n");
-            longline(num);
-            printf("\n");
-            break;
+        if(argc == 3){
+            int aux = atoi(argv[2]);
+            if(aux > 0){
+                int lineas = atoi(argv[2]); //Seleccionamos el número de líneas a procesar    
+            }
+            else{
+                printf("ERROR --> El numero de lineas introducido no es valido\n");
+                return 1;
+            }
+            
         }
-        opc = menu();
+
+        if(funcionSel == "-head"){
+            head(lineas);
+            return 0;
+        }
+        else if(funcionSel == "-tail"){
+            tail(lineas);
+            return 0;
+        }
+        else if(funcionSel == "-longlines"){
+            longlines(lineas);
+            return 0;
+        }
+        else{
+            printf("ERROR --> La funcion introducida no se reconoce\n");
+            return 1;
+        }
     }
+
 }
