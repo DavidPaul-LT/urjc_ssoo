037a139f744cbc71f68d5fc31188a03cadeaa965
David Paul Limaylla Ticlavilca <116211485+DavidPaul-LT@users.noreply.github.com>
2023-11-16 21:36:46 +0100

    Update test.c comentarios añadidos

--- test.c
+++ test.c
@@ -3,25 +3,26 @@
 #include <string.h>
 
 int main(int argc, char const *argv[], char *envp[]){
-
-    if(argc < 2 || argc > 3){ //Control de errores en el caso de que los parámetros sean erroneos
+    //Control de errores en el caso de que los parámetros sean erroneos
+    if(argc < 2 || argc > 3){
         printf("ERROR --> Numero de argumentos erroneo\n");
         return 1;
     } 
     else{
+        // Verificamos que el primer argumento comience con '-'
         if(argv[1][0] != '-'){
             printf("ERROR --> Argumeto invalido\n");
             return 1;
         }
         else{
             char funcionSel[10];
-            strcpy(funcionSel, argv[1]);
+            strcpy(funcionSel, argv[1]);                           // Almacenamos el primer argumento en 'funcionSel'
             int lineas;
-
+            // Verificamos si hay un tercer argumento (número de líneas)
             if(argc == 3){
                 int aux = atoi(argv[2]);
                 if(aux > 0){
-                    lineas = atoi(argv[2]); //Seleccionamos el número de líneas a procesar
+                    lineas = atoi(argv[2]);                         //Seleccionamos el número de líneas a procesar
                     printf("%i \n", lineas);
                 }
                 else{
@@ -29,8 +30,9 @@ int main(int argc, char const *argv[], char *envp[]){
                     return 1;
                 }
             }else{
-                lineas = 10;
+                lineas = 10;                                        // Si no se proporciona un tercer argumento, establecemos el valor predeterminado en 10
             }
+            // Comparamos 'funcionSel' con las opciones y llamamos a la función correspondiente
             if(strcmp(funcionSel, "-head")==0){
                 head(lineas);
                 return 0;
