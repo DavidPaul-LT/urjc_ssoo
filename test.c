b3c51b78cd00dade7cf652b15442e7e5d530c83f
David Paul Limaylla Ticlavilca <116211485+DavidPaul-LT@users.noreply.github.com>
2023-11-16 18:23:47 +0100

    Update test.c
    Solución a problemas de comparación
     "-", "-<funcion>"

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include "libreria.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[], char *envp[]){
 
@@ -7,35 +8,41 @@ int main(int argc, char const *argv[], char *envp[]){
         printf("ERROR --> Numero de argumentos erroneo\n");
         return 1;
     } 
-    else{   
-        if(argv[1][0] != "-"){
+    else{
+        if(argv[1][0] != '-'){
+            printf("%c \n", argv[1][0]);
             printf("ERROR --> Argumeto invalido");
             return 1;
         }
         else{
-            char funcionSel = argv[1][strlen(argv[1])]; //Seleccionamos la función introducida
-            int lineas = 10; //El número de líneas a procesar por defecto es 10
+            char funcionSel[10];
+            strcpy(funcionSel, argv[1]);
+            int lineas;
 
             if(argc == 3){
                 int aux = atoi(argv[2]);
                 if(aux > 0){
-                    int lineas = atoi(argv[2]); //Seleccionamos el número de líneas a procesar    
+                    lineas = atoi(argv[2]); //Seleccionamos el número de líneas a procesar
+                    printf("%i \n", lineas);
                 }
                 else{
-                    printf("ERROR --> El numero de lineas introducido no es valido\n");
+                    printf("ERROR --> El número de lineas introducido no es válido\n");
                     return 1;
                 }
+            }else{
+                lineas = 10;
+                printf("%i", lineas);
             }
 
-            if(funcionSel == "-head"){
+            if(strcmp(funcionSel, "-head")==0){
                 head(lineas);
                 return 0;
             }
-            else if(funcionSel == "-tail"){
+            else if(strcmp(funcionSel, "-tail")==0){
                 tail(lineas);
                 return 0;
             }
-            else if(funcionSel == "-longlines"){
+            else if(strcmp(funcionSel, "-longlines")==0){
                 longlines(lineas);
                 return 0;
             }
